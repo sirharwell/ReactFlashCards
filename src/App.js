8dc6ea05c5ec01a1689ecb85d2959221e138cc28
Ian Harwell <sirharwell@gmail.com>
2018-03-07 16:37:04 -0700

    css

--- src/App.js
+++ src/App.js
@@ -8,11 +8,25 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Flash Cards React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+Flash Cards!        </p>
+        <body class="background center">
+          <div class="flip3d">
+            <div class="back">webpack is a module bundler. Its main purpose is to bundle JavaScript files for usage in a browser, yet it is also capable of transforming, bundling, or packaging just about any resource or asset.</div>
+            <div class="front">What is Webpack?</div>
+          </div>
+          <div class="flip3d">
+            <div class="back">yarn add webpack-dev-server --dev</div>
+            <div class="front">How do you add a webpack server?</div>
+          </div>
+          <div class="flip3d">
+            <div class="back">Rest parameters allow you to accept an infinite number of arguments.  Rest params are indicated by using 3 dots and a name.</div>
+            <div class="front">What is a rest param?</div>
+          </div>
+          <br />
+          </body>
       </div>
     );
   }

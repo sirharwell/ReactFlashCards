a1b4f9814eb0524d489b0c35d57696fd059fc4be
Ian Harwell <sirharwell@gmail.com>
2018-03-07 17:32:30 -0700

    forms

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FlashForm from './FlashForm';
 
 class App extends Component {
+  state = {
+    flashes: [],
+  }
+
+  getId = () => {
+  //NOTE We are just using this as a helper function for id's since we aren't using a db yet
+ return Math.floor((1 + Math.random()) * 0x10000)
+   .toString(16)
+   .substring(1);
+}
+
+  addFlash = (name) => {
+  const { flashes } = this.state;
+  const flash = { name, id: this.getId(), complete: false }
+  this.setState({ flashs: [flash, ...flashes] })
+}
+
   render() {
     return (
       <div className="App">
@@ -25,11 +43,14 @@ Flash Cards!        </p>
             <div class="back">Rest parameters allow you to accept an infinite number of arguments.  Rest params are indicated by using 3 dots and a name.</div>
             <div class="front">What is a rest param?</div>
           </div>
-          <br />
           </body>
+          <br />
+          <FlashForm addItem={this.addFlash}/>
       </div>
     );
   }
 }
+
+
 
 export default App;
---
+++ src/Card.js
@@ -0,0 +1,6 @@
+require React from 'react';
+
+<div class="flip3d">
+  <div class="back">answer</div>
+  <div class="front">question?</div>
+</div>
